1b3830717fa76b3b328b8a42addb39d53693eddb
Jakub Josef Forman <me@jakubforman.eu>
2022-09-29 11:28:33 +0200

    Project init lesson
    - adding API service
    - adding DEV environments
    - adding temporary code for tab1 (home) with design
    - adding HTTP Client (& Module)

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+
+export interface WeatherCoord{
+  lon: number;
+  lat: number;
+}
+
+export interface Weather {
+  id: number;
+  name: string;
+  cod: number;
+  base: string;
+  weather: { description: string }[];
+  clouds: {
+    all: number;
+  };
+  coord: WeatherCoord;
+  visibility: string;
+  rain?: {
+    [type: string]: number;
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  /**
+   * Get weather from API
+   *
+   * @param latitude
+   * @param longitude
+   */
+  getWeather(latitude: number, longitude: number): Observable<Weather> {
+    const url = '/weather?units=metric&lat=' + latitude + `&lon=${longitude}&appid=${environment.api.key}`;
+    return this.http.get<Weather>(environment.api.baseUrl + url);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ApiService, Weather} from '../services/api/api.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  /**
+   * Weather data
+   */
+  weather$: Observable<Weather>;
+
+  constructor(
+    // get custom Service from DI
+    private apiService: ApiService
+  ) {
+    // set property (GEO Zlín - Tečovice)
+    this.weather$ = this.apiService.getWeather(49.2310213, 17.6064677);
+
+    // other way
+    /*this.apiService.getWeather(49.2310213, 17.6064677).subscribe(data => {
+      this.weather = data // need modified implementation in view
+    });*/
+  }
 
 }
